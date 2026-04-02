a3ab9295f386c97095a55912dd20a115b92d783c
MarcosLú <marcoslu.dev.web@gmail.com>
2026-04-01 23:39:57 -0300

    Estilizando o drawer na página Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,18 +3,15 @@ import { View, Text, Button } from "react-native";
 
 import { AuthContext } from "../../contexts/auth";
 
+import Header from "../../../components/Header";
+import { Background } from "./styles";
+
 export default function Home(){
     const { signOut, user } = useContext(AuthContext)
     return(
-        <View>
-            <Text>  Tela Home </Text>
-            <Text>  Nome: {user.name} </Text>
-
-            <Button
-            title="Sair da conta"
-            onPress={() => signOut()}
-            />
-        </View>
+        <Background>
+            <Header title='Minhas movimentações'/>
+        </Background>
 
     )
 }
